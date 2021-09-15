57ca686f9567487cd973f21b98f43d7937ada430
Pavel Sheremet <pashqa.sheremet@gmail.com>
2021-09-15 16:57:48 +0300

    change console on the task 1

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,7 @@ console.log(`В списке ${categoriesListItems.length} категории`);
 const printCategoryDescription = item => {
   const itemTitleName = item.querySelector('h2').textContent;
   const itemListItemsLength = item.querySelectorAll('li').length;
-  console.log([
+  console.table([
     `Категория: ${itemTitleName}`,
     `Количество элементов: ${itemListItemsLength}`,
   ]);
